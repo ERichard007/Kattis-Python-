1b88b03801e9e41294082bd6ef354a9f10a56fc7
evan-richard <ecrb29@umsystem.edu>
2025-02-25 19:20:17 -0600

    added

---
+++ Heimilisverk.py
@@ -0,0 +1,18 @@
+numChores = int(input())
+setOfChores = set()
+
+for x in range(numChores):
+    print("\n")
+    newChore = str(input())
+    setOfChores.add(newChore)
+
+    print("\nCurrent:")
+    for chore in setOfChores:
+        print(chore)
+
+print("\n\n\nOUTPUT:")
+
+reverseChores = list(setOfChores)
+reverseChores.
+for chore in reverseChores:
+    print(chore)
