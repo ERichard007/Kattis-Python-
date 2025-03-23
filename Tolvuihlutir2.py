a44bba2f1bb2b296729031d4430afbde3d744ba9
ERichard007 <ecrb29@umsystem.edu>
2025-03-22 21:21:21 -0500

    Finally Finnished Tolvuiwhatever w/ BINARY SEACH!

--- Tolvuihlutir2.py
+++ Tolvuihlutir2.py
@@ -10,4 +10,37 @@ for x in range(availableComponents):
     highPerf = max(highPerf, int(a[2]))
     componentNames[a[0]].append((int(a[1]), int(a[2])))
 
-print(f"{componentNames} + {highPerf}")
+bestPerf = -1
+while lowPerf <= highPerf:
+    budget = moneyAvailable
+    midPerf = lowPerf + (highPerf - lowPerf) // 2
+    print(midPerf)
+
+    possible = True
+    for comp, priceAndPerf in componentNames.items():
+        minPrice = float('inf')
+        for price, perf in priceAndPerf:
+            if perf >= midPerf:
+                minPrice = min(minPrice, price)
+
+        if minPrice == float('inf'):
+            possible = False
+            break
+        else:
+            budget -= minPrice
+
+    if possible and budget >= 0:
+        lowPerf = midPerf + 1
+        bestPerf = midPerf
+    else:
+        highPerf = midPerf - 1
+
+if bestPerf != -1:
+    print(bestPerf)
+else:
+    print("O nei!")
+
+    
+            
+
+        
