72dd74c83b47f484c0c6e0a153dcd4415034f0ff
ERichard007 <ecrb29@umsystem.edu>
2025-04-03 10:56:51 -0500

    DingDongDitchBitch

--- DingDongDitch2.0.py
+++ DingDongDitch2.0.py
@@ -1,13 +1,16 @@
-import heapq
-
 houses, friends = map(int, input().split())
 angerLevels = list(map(int, input().split()))
 housesCanRing = list(map(int, input().split()))
 
-heapq.heapify(angerLevels)
-for x in range(len(angerLevels)):
+angerLevels.sort()
+sum = 0
+for i in range(houses):
+    sum += angerLevels[i]
+    angerLevels[i] = sum
 
-print(f"** {houses} {friends} {angerLevels} {housesCanRing} **\n")
-
+for i in range(friends):
+    canRing = housesCanRing[i]
+    print(angerLevels[canRing-1])
+        
 
 
